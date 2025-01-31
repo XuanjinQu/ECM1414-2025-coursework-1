f65cb511bc984835074c822648d6f66dbce4bd7e
Jacob <77414989+Oxyn4@users.noreply.github.com>
2025-01-31 10:05:34 +0000

    Add function to load building data. Check paths existence

---
+++ algorithm.py
@@ -0,0 +1,7 @@
+import json 
+import os
+
+# load the data from a file
+def loadBuildingData(file_path: os.path):
+    if not file_path.exists():
+        raise RuntimeError("Building data file does not exist")
