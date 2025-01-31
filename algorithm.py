bfbf2e5d65c3bbdb99c8b712032232ab0af38799
Jacob <77414989+Oxyn4@users.noreply.github.com>
2025-01-31 10:10:09 +0000

    Add main function

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,10 @@
-import json 
 import os
 
 # load the data from a file
 def loadBuildingData(file_path: os.path):
     if not file_path.exists():
         raise RuntimeError("Building data file does not exist")
+    
+
+if __name__ == "__main__":
+    pass
