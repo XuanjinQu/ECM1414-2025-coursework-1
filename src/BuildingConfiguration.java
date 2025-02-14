297e49fe712c2472c400ac7d67f3d176bb4c314b
jacob evans <77414989+Oxyn4@users.noreply.github.com>
2025-02-14 11:55:24 +0000

    rewrote BuildingConfiguration class fixing numerous syntax errors

--- src/BuildingConfiguration.java
+++ src/BuildingConfiguration.java
@@ -4,29 +4,25 @@ import java.util.Random;
 class Floor {
     //  each object represents a number of people
     // the value of each object represents the floor number that person wants to travel to
-    java.util.Vector<int> FloorRequests;
+    public java.util.Vector<Integer> FloorRequests;
 }
 
 // Loads a Building configuration Object from a file path
-class BuildingConfiguraiton {
+class BuildingConfiguration {
     // contains any number of floors
-    java.util.Vector<Floor> Floors;
+    public java.util.Vector<Floor> Floors;
 
-    // java.nio.file.Paths is a specialised object for storing file paths
-    // prevents errors by ensuring the paths are valid
-    BuildingConfiguraiton() {}
-
-    public static BuildingConfiguration FromRandom(uint MaxFloors = 10, uint MaxPeoplePerFloor = 10) {
+    public static BuildingConfiguration FromRandom(int MaxFloors, int MaxPeoplePerFloor) {
         Random rand = new Random();
 
-        BuildingConfiguraiton Configuration = new BuildingConfiguraiton();
+        BuildingConfiguration Configuration = new BuildingConfiguration();
 
         int AmountOfFloors = rand.nextInt(MaxFloors);
 
         for (int i = 0; i < AmountOfFloors; i++) {
             Floor floor = new Floor();
 
-            AmountOfPeople = rand.nextInt(MaxPeoplePerFloor);
+            int AmountOfPeople = rand.nextInt(MaxPeoplePerFloor);
 
             for (int j = 0; i < AmountOfPeople; i++) {
                 int Destination = rand.nextInt(MaxFloors);
@@ -34,7 +30,9 @@ class BuildingConfiguraiton {
                 floor.FloorRequests.addElement(Destination);
             }
 
-            BuildingConfiguraiton.Floors.addElement(floor);
+            Configuration.Floors.addElement(floor);
         }
+
+        return Configuration;
     }
 }
