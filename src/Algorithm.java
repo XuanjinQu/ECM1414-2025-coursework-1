19801b156e753d6feac7dbc863a35f7490f46b31
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-02-20 23:36:10 +0000

    Add toString methods for Floor and BuildingConfiguration, reworked random generation

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,6 +1,8 @@
 
 class Algorithm {
     public static void main(String[] args) {
+        BuildingConfiguration Building = BuildingConfiguration.FromRandom(10, 5);
 
+        System.out.println(Building);
     }
 }
--- src/BuildingConfiguration.java
+++ src/BuildingConfiguration.java
@@ -1,16 +1,59 @@
 
+import java.util.ArrayList;
 import java.util.Random;
 
-class Floor {
+final class Floor {
     //  each object represents a number of people
     // the value of each object represents the floor number that person wants to travel to
-    public java.util.ArrayList<Integer> FloorRequests;
+    java.util.ArrayList<Integer> FloorRequests = new ArrayList<Integer>();
+
+    public java.util.ArrayList<Integer> GetFloorRequests() {
+        return FloorRequests;
+    }
+
+    private void AddFloorRequest(int floor) {
+        FloorRequests.add(floor);
+    }
+
+    public static Floor FromRandom(int MaxDestination, int MaxNumberOfRequests) {
+        Random rand = new Random();
+
+        Floor floor = new Floor();
+
+        int NumberofRequests = rand.nextInt(MaxNumberOfRequests);
+
+        for (int i = 0; i < NumberofRequests; i++) {
+            floor.AddFloorRequest(rand.nextInt(MaxDestination));
+        }
+
+        return floor;
+    }
+
+    public String toString() {
+        StringBuilder ret = new StringBuilder("floor = {");
+        for (int i = 0; i < FloorRequests.size(); i++) {
+            ret.append(FloorRequests.get(i));
+            if (i != FloorRequests.size() - 1) {
+                ret.append(", ");
+            }
+        }
+        ret.append("}");
+        return ret.toString();
+    }
 }
 
 // Loads a Building configuration Object from a file path
-class BuildingConfiguration {
+final class BuildingConfiguration {
     // contains any number of floors
-    public java.util.ArrayList<Floor> Floors;
+    java.util.ArrayList<Floor> Floors  = new java.util.ArrayList<Floor>();
+
+    public java.util.ArrayList<Floor> GetFloors() {
+        return Floors;
+    }
+
+    private void AddFloor(Floor floor) {
+        Floors.add(floor);
+    }
 
     public static BuildingConfiguration FromRandom(int MaxFloors, int MaxPeoplePerFloor) {
         Random rand = new Random();
@@ -20,19 +63,23 @@ class BuildingConfiguration {
         int AmountOfFloors = rand.nextInt(MaxFloors);
 
         for (int i = 0; i < AmountOfFloors; i++) {
-            Floor floor = new Floor();
+            Floor floor = Floor.FromRandom(MaxFloors, MaxPeoplePerFloor);
 
-            int AmountOfPeople = rand.nextInt(MaxPeoplePerFloor);
-
-            for (int j = 0; i < AmountOfPeople; i++) {
-                int Destination = rand.nextInt(MaxFloors);
-
-                floor.FloorRequests.add(Destination);
-            }
-
-            Configuration.Floors.add(floor);
+            Configuration.AddFloor(floor);
         }
 
         return Configuration;
+    }
+
+    public String toString() {
+        StringBuilder ret = new StringBuilder("Building[floors = {");
+        for (int i = 0; i < Floors.size(); i++) {
+            ret.append(Floors.get(i).toString());
+            if (i != Floors.size() - 1) {
+                ret.append(", ");
+            }
+        }
+        ret.append("}]");
+        return ret.toString();
     }
 }
