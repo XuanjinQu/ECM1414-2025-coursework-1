571ae7e745b58f35cfd8757fd204d41d35d0ea97
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-02-21 01:26:12 +0000

    add branch for scan development

--- src/Look.java
+++
@@ -1,9 +0,0 @@
-public class Look extends Algorithm {
-    public Look(Building buildingState) {
-        super(buildingState);
-    }
-
-    public Building NextStep() {
-        return super.building;
-    }
-}
